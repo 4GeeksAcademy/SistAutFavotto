115815c149d802ab93e40ab88dce10fe560a47a7
Aless <titifavotto@gmail.com>
2025-01-24 16:38:03 +0000

    casi

--- src/api/commands.py
+++ src/api/commands.py
@@ -1,34 +1,114 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
-import click
-from api.models import db, User
+db = SQLAlchemy()
 
-"""
-In this file, you can add as many commands as you want using the @app.cli.command decorator
-Flask commands are usefull to run cronjobs or tasks outside of the API but sill in integration 
-with youy database, for example: Import the price of bitcoin every night as 12am
-"""
-def setup_commands(app):
+favorite_planets = db.Table('favorite_planets',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('planet_id', db.Integer, db.ForeignKey('planet.id'), primary_key=True),
+)
+favorite_persons = db.Table('favorite_persons',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('person_id', db.Integer, db.ForeignKey('person.id'), primary_key=True),
+)
+favorite_vehicles = db.Table('favorite_vehicles',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('vehicle_id', db.Integer, db.ForeignKey('vehicle.id'), primary_key=True),
+)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)  # Make sure to accommodate hashed length
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    person_favorite = db.relationship('Person', secondary=favorite_persons, backref=db.backref('favorited_by_users', lazy=True))
+    planet_favorite = db.relationship('Planet', secondary=favorite_planets, backref=db.backref('favorited_by_users', lazy=True))
+    vehicle_favorite = db.relationship('Vehicle', secondary=favorite_vehicles, backref=db.backref('favorited_by_users', lazy=True))
+
+    def __repr__(self):
+        return '<User %r>' % self.email
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # "password": self.password,
+            "is_active": self.is_active,
+            # do not serialize the password, it's a security breach
+        }
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.Float, nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Person %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+        }
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    length = db.Column(db.Float, nullable=False)
+    crew = db.Column(db.Integer, nullable=False)
+    max_atmosphering_speed = db.Column(db.Integer, nullable=False)
+    cargo_capacity  = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "vehicle_class": self.vehicle_class,
+            "length": self.length,
+            "crew": self.crew,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "cargo_capacity": self.cargo_capacity,
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    population = db.Column(db.BigInteger, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
     
-    """ 
-    This is an example command "insert-test-users" that you can run from the command line
-    by typing: $ flask insert-test-users 5
-    Note: 5 is the number of users to add
-    """
-    @app.cli.command("insert-test-users") # name of our command
-    @click.argument("count") # argument of out command
-    def insert_test_users(count):
-        print("Creating test users")
-        for x in range(1, int(count) + 1):
-            user = User()
-            user.email = "test_user" + str(x) + "@test.com"
-            user.password = "123456"
-            user.is_active = True
-            db.session.add(user)
-            db.session.commit()
-            print("User: ", user.email, " created.")
+    def __repr__(self):
+        return '<Planet %r>' % self.name
 
-        print("All test users created")
-
-    @app.cli.command("insert-test-data")
-    def insert_test_data():
-        pass
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "orbital_period": self.orbital_period,
+            "rotation_period": self.rotation_period,
+            "diameter": self.diameter,
+        }
